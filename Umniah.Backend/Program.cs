942cc54c26915ecabdb1d0d4c3f86664c478d324
Sanguine-dreams <sarah0redqueen@gmail.com>
2025-11-09 14:23:07 +0300

    Refactor Program.cs to register services and add CORS support; update project paths in various configuration files

--- Umniah.Backend/Program.cs
+++ Umniah.Backend/Program.cs
@@ -1,23 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using Umniah.Backend.Data.Context;
+using Umniah.Backend.DTOs.Input;
+using Umniah.Backend.DTOs.Output;
+using Umniah.Backend.Interfaces;
+using Umniah.Backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
+// Add DbContext - move this before builder.Build()
 builder.Services.AddDbContext<UmniahDbContext>(
-    options => { options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnectionString"]); },
+    options => options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnectionString"]),
     ServiceLifetime.Scoped);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-// Add services to the container.
 
+// Register Services
+builder.Services.AddScoped<ICrudService<InputGalleryImage, OutputGalleryImage>, GalleryImageService>();
+builder.Services.AddScoped<ICrudService<InputProduct, OutputProduct>, ProductService>();
+builder.Services.AddScoped<ICrudService<InputPurchase, OutputPurchase>, PurchaseService>();
+builder.Services.AddScoped<ICrudService<InputSale, OutputSale>, SaleService>();
+builder.Services.AddScoped<ICrudService<InputOrder, OutputOrder>, OrderService>();
+builder.Services.AddScoped<IBulkCrudService<InputSeller, OutputSeller>, SellerService>();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+// Add Controllers
 builder.Services.AddControllers();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,5 +53,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Use CORS
+app.UseCors("AllowAll");
+
+// Add authentication & authorization middleware if needed
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Map controllers
+app.MapControllers();
 
 app.Run();
--- Umniah.Backend/obj/Debug/net8.0/Umniah.Backend.AssemblyInfo.cs
+++ Umniah.Backend/obj/Debug/net8.0/Umniah.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Umniah.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e8a8ca8a4534ee979f409c530b7d77335eea1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451085911fb7a047d75bf333f4c8386a18853a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Umniah.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Umniah.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
